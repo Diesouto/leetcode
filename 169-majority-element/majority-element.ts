1803af20eb3847a30e8b51f838a898171d7ec3e4
Diesouto <diegosouto2000@gmail.com>
2024-09-09 14:23:46 +0200

    Time: 61 ms (75.55%) | Memory: 53.7 MB (73.71%) - LeetSync

--- 169-majority-element/majority-element.ts
+++ 169-majority-element/majority-element.ts
@@ -1,14 +1,13 @@
 function majorityElement(nums: number[]): number {
-    let count = 0
-    let candidate = null
+    let count = 0;
+    let candidate = null;
 
     for (let num of nums) {
-        if(count === 0) {
-            candidate = num
+        if (count === 0) {
+            candidate = num;
         }
-
         count += (num === candidate) ? 1 : -1;
     }
 
-    return candidate
+    return candidate;
 }
